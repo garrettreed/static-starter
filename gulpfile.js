883f71903c9e94e8b7470a744fd7cd867cfed9eb
Garrett Reed <garrett@garrettreed.info>
2019-10-06 10:45:26 -0400

    Add asset rev tasks

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,22 @@ const autoprefixer = require("gulp-autoprefixer");
 const mustache = require("gulp-mustache");
 const concat = require("gulp-concat");
 const gulpif = require("gulp-if");
+const rev = require("gulp-rev");
+const clean = require("gulp-clean");
+const revRewrite = require("gulp-rev-rewrite");
 
 const isProdEnv = process.env.ENVIRONMENT === "production";
-console.log(`Building for ${process.env.ENVIRONMENT}`);
+console.log(`Acting on ${process.env.ENVIRONMENT} environment.`);
 
+const distPath = "public/dist/";
 const paths = {
     styles: {
         src: ["libs/**/*.css", "styles/**/*.scss"],
-        dest: "public/dist/"
+        dest: distPath
     },
     scripts: {
         src: ["libs/**/*.js", "js/**/*.js"],
-        dest: "public/dist/"
+        dest: distPath
     },
     templates: {
         src: ["templates/**/*.mustache", "templates/**/*.html", "!templates/partials/**"],
@@ -59,9 +63,35 @@ function templates() {
         .pipe(gulp.dest(paths.templates.dest));
 }
 
+function buildManifest() {
+    return gulp
+        .src(`${distPath}/*.{js,css}`)
+        .pipe(rev())
+        .pipe(gulp.dest(distPath))
+        .pipe(
+            rev.manifest(`${distPath}/rev-manifest.json`, {
+                base: distPath,
+                merge: true
+            })
+        )
+        .pipe(gulp.dest(distPath));
+}
+
+function writeManifest() {
+    return gulp
+        .src(`${paths.templates.dest}**/*.html`)
+        .pipe(revRewrite({ manifest: gulp.src("public/dist/rev-manifest.json") }))
+        .pipe(gulp.dest(paths.templates.dest));
+}
+
+function cleanDist() {
+    return gulp.src(`${distPath}/*.{js,css,map}`, { read: false }).pipe(clean());
+}
+
 function watch() {
     gulp.watch(paths.scripts.src, scripts).on("change", browserSync.reload);
     gulp.watch(paths.styles.src, styles);
+    gulp.watch("./data.json", templates).on("change", browserSync.reload);
     gulp.watch(paths.templates.src, templates).on("change", browserSync.reload);
 }
 
@@ -77,11 +107,19 @@ function serve() {
     watch();
 }
 
-const build = gulp.parallel(styles, scripts, templates);
+const build = gulp.series(
+    cleanDist,
+    gulp.parallel(styles, scripts, templates),
+    buildManifest,
+    writeManifest
+);
+
+const dev = gulp.series(cleanDist, gulp.parallel(styles, scripts, templates), serve);
 
 exports.styles = styles;
 exports.scripts = scripts;
 exports.templates = templates;
 exports.watch = watch;
 exports.build = build;
-exports.default = gulp.series(build, serve);
+exports.dev = dev;
+exports.default = dev;
